3e97a0715fabe605028ce072687072b56ac1952f
Sukh <offcial@User.Com>
2023-12-08 09:45:36 +0530

    change login image

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,10 +6,23 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Image.asset("assets/images/undraw_Authentication_re_svpt.png"),
-      ],
-    ));
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_image.png",
+
+            fit: BoxFit.cover,
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:learning2/Pages/Home_Page.dart';
 import 'package:learning2/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -13,13 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-
-   routes: {
-        "/":(context)=> const LoginPage(),
-        "/home":(context)=> const HomePage(),
-     "/Login":(context)=>const LoginPage(),
-    },
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/Login": (context) => const LoginPage(),
+      },
     );
-      }
+  }
 }
